83f8cb2cacff76a0685c0d12a5c213bc3e5e8e14
aleiton <aleiton.design@gmail.com>
2020-06-08 11:08:29 -0300

    Display db messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,8 @@ User.create(username: "Jonsnow", password: "password")
 User.create(username: "Arya", password: "password")
 User.create(username: "Frodo", password: "password")
 User.create(username: "Gandalf", password: "password")
+
+Message.create(body: "Hello everyone", user_id: 1)
+Message.create(body: "I work in an office", user_id: 3)
+Message.create(body: "Oh to talking improve produce in limited offices fifteen an", user_id: 4)
+Message.create(body: "In up so discovery my middleton eagerness dejection explained", user_id: 3)
